ba680e029f5dece6fd6dc9aea50a631d26e78d40
rama-rahimov <r17654974@gmail.com>
2024-01-09 23:59:55 +0400

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Training from './Training';
-import Vpereed from './Vpereed';
+import Training from './Training';
+// import Vpereed from './Vpereed';
 // import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-    <Vpereed />
+    <Training />
     </>
 );
 
